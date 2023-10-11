4f610cc191505fc403a3713c9f95e073fa7996dc
gmalkar1 <gnaneshwarreddy.malkareddy1@t-mobile.com>
2023-10-11 15:22:07 -0500

    chore: Undo add and remove from reading list

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -1,14 +1,57 @@
+import { $, $$, ExpectedConditions, browser } from 'protractor';
+
+import { doesNotThrow } from 'assert';
+
 describe('When: I use the reading list feature', () => {
-  beforeEach(() => {
-    cy.startAt('/');
+  it('Then: I should see my reading list', async () => {
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+    );
+
+    const readingListToggle = await $('[data-testing="toggle-reading-list"]');
+    await readingListToggle.click();
+
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement(
+        $('[data-testing="reading-list-container"]'),
+        'My Reading List'
+      )
+    );
   });
 
-  it('Then: I should see my reading list', () => {
-    cy.get('[data-testing="toggle-reading-list"]').click();
-
-    cy.get('[data-testing="reading-list-container"]').should(
-      'contain.text',
-      'My Reading List'
+  it('Then: I should be able to undo after removing a book from the reading list', async () => {
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
     );
+
+    const form = await $('form');
+    const input = await $('input[type="search"]');
+    await input.sendKeys('javascript');
+    await form.submit();
+
+    const bookItems = $$('[data-testing="book-item"]');
+    const itemsCount = await bookItems.count();
+    const count = itemsCount > 2 ? 2 : itemsCount;
+    for (let i = 0; i < count; i++) {
+      const bookItem = await bookItems.get(i);
+      const addBtn = await bookItem.$('button[data-testing="want-to-read"]');
+      await addBtn.click();
+    }
+
+    const readingListToggle = await $('[data-testing="toggle-reading-list"]');
+    await readingListToggle.click();
+
+    const readingListItems = $$('[data-testing="reading-list-item"]');
+    const readingListItem = await readingListItems.get(0);
+    const removeBtn = await readingListItem.$('button[data-testing="remove-reading-list-item"]');
+    await removeBtn.click();
+
+    ExpectedConditions.textToBePresentInElement(
+      $('.tmo-snack-bar'),
+      'Removed book from the reading list!'
+    )
+
   });
 });

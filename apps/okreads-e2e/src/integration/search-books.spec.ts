fea6508e88ebc2d55eadda8f47251679dca221d6
gmalkar1 <gnaneshwarreddy.malkareddy1@t-mobile.com>
2023-10-10 19:17:38 -0500

    chore: adding instant search

--- apps/okreads-e2e/src/integration/search-books.spec.ts
+++ apps/okreads-e2e/src/integration/search-books.spec.ts
@@ -1,17 +1,35 @@
+import { $, $$, ExpectedConditions, browser } from 'protractor';
+
+import { expect } from 'chai';
+
 describe('When: Use the search feature', () => {
-  beforeEach(() => {
-    cy.startAt('/');
+  it('Then: I should be able to search books by title', async () => {
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+    );
+
+    const form = await $('form');
+    const input = await $('input[type="search"]');
+    await input.sendKeys('javascript');
+    await form.submit();
+
+    const items = await $$('[data-testing="book-item"]');
+    expect(items.length).to.be.greaterThan(1, 'At least one book');
   });
 
-  it('Then: I should be able to search books by title', () => {
-    cy.get('input[type="search"]').type('javascript');
+  it('Then: I should see search results as I am typing', async () => {
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+    );
 
-    cy.get('form').submit();
+    const form = await $('form');
+    const input = await $('input[type="search"]');
+    await input.sendKeys('javascript');
 
-    cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);
-  });
+    const items = await $$('[data-testing="book-item"]');
+    expect(items.length).to.be.greaterThan(1, 'At least one book');
 
-  xit('Then: I should see search results as I am typing', () => {
-    // TODO: Implement this test!
   });
 });
